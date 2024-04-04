464f95617a81bc6821bae95a0ecd97b5fb0f154a
Callum Jones <cj@icj.me>
2024-04-04 20:27:12 +0100

    use a table layout

--- go.mod
+++ go.mod
@@ -5,11 +5,12 @@ go 1.17
 require (
 	github.com/dustin/go-humanize v1.0.0
 	github.com/gorilla/mux v1.8.0
-	github.com/multiplay/go-ts3 v1.0.1
+	github.com/multiplay/go-ts3 v1.2.0
 	github.com/sirupsen/logrus v1.8.1
 )
 
 require (
-	github.com/mitchellh/mapstructure v1.4.3 // indirect
-	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/dustin/go-humanize"
 	"github.com/gorilla/mux"
 	"github.com/multiplay/go-ts3"
 	"github.com/sirupsen/logrus"
@@ -24,106 +23,29 @@ var (
 	serverID = os.Getenv("TS3_SERVERID")
 )
 
-type Channel struct {
-	ID                   int    `ms:"cid"`
-	ParentID             int    `ms:"pid"`
-	ChannelOrder         int    `ms:"channel_order"`
-	ChannelName          string `ms:"channel_name"`
-	TotalClients         int    `ms:"total_clients"`
-	NeededSubscribePower int    `ms:"channel_needed_subscribe_power"`
-}
-
-type ClientInfo struct {
-	ID         int    `ms:"clid"`
-	DatabaseID int    `ms:"client_database_id"`
-	Nickname   string `ms:"client_nickname"`
-	Type       int    `ms:"client_type"`
-
-	ChannelID int `ms:"cid"`
-
-	// ClientIdleTime is the time the client has been idle.
-	ClientIdleTime int `ms:"client_idle_time"`
-	// Version is the client version.
-	Version string `ms:"client_version"`
-	// Platform is the client platform.
-	Platform string `ms:"client_platform"`
-	// InputMuted indicates if the client mic is muted.
-	InputMuted bool `ms:"client_input_muted"`
-	// OutputMuted indicates if the client speakers are muted.
-	OutputMuted bool `ms:"client_output_muted"`
-	// OutputOnlyMuted - ?
-	OutputOnlyMuted bool `ms:"client_outputonly_muted"`
-	// HasInputHardware indicates if the client has a mic.
-	HasInputHardware bool `ms:"client_input_hardware"`
-	// HasOutputHardware indicates if the client has a mic.
-	HasOutputHardware bool `ms:"client_output_hardware"`
-	// DefaultChannelName is the name of the client's default channel.
-	DefaultChannelName string `ms:"client_default_channel"`
-	// IsRecording indicates if the client is recording.
-	IsRecording bool `ms:"client_is_recording"`
-	// ChannelGroupId is the ID of the channel group the client is in.
-	ChannelGroupId int `ms:"client_channel_group_id"`
-	// ServerGroups is the list of server groups on the client.
-	// ServerGroups []int `ms:"client_servergroups"`
-	// TotalConnections is the total number of times the client has connected.
-	TotalConnections int `ms:"client_totalconnections"`
-	// Away is set if the client is marked as away.
-	Away bool `ms:"client_away"`
-	// AwayMessage is the client away message.
-	AwayMessage string `ms:"client_away_message"`
-	// TalkPower is the talk power of the client.
-	TalkPower int `ms:"client_talk_power"`
-	// TalkRequest indicates if the client has requested to talk.
-	TalkRequest bool `ms:"client_talk_request"`
-	// TalkRequestMessage is the message the client gave when requesting to talk.
-	TalkRequestMessage string `ms:"client_talk_request_msg"`
-	// Description is the description of the client.
-	Description string `ms:"client_description"`
-	// IsTalker indicates if the client is a talker.
-	IsTalker bool `ms:"client_is_talker"`
-	// MonthBytesUploaded is the number of bytes uploaded this month.
-	MonthBytesUploaded int `ms:"client_month_bytes_uploaded"`
-	// MonthBytesDownloaded is the number of bytes downloaded this month.
-	MonthBytesDownloaded int `ms:"client_month_bytes_downloaded"`
-	// TotalBytesUploaded is the number of bytes uploaded total.
-	TotalBytesUploaded int `ms:"client_total_bytes_uploaded"`
-	// TotalBytesDownloaded is the number of bytes downloaded total.
-	TotalBytesDownloaded int `ms:"client_total_bytes_downloaded"`
-	// IsPrioritySpeaker indicates if the client is a priority speaker
-	IsPrioritySpeaker bool `ms:"client_is_priority_speaker"`
-	// PhoneticNickname is the phonetic nickname if given.
-	PhoneticNickname string `ms:"client_nickname_phonetic"`
-	// NeededmsViewPower is the view power necessary to view the client.
-	NeededmsViewPower int `ms:"client_needed_ms_view_power"`
-	// IconId is the icon ID of the client.
-	IconId int `ms:"client_icon_id"`
-	// IsChannelCommander indicates if the client is a channel commander.
-	IsChannelCommander bool `ms:"is_channel_commander"`
-}
-
-type OnlineClient struct {
-	ID          int    `ms:"clid"`
-	DatabaseID  int    `ms:"client_database_id"`
-	Nickname    string `ms:"client_nickname"`
-	Type        int    `ms:"client_type"`
-	Away        bool   `ms:"client_away"`
-	AwayMessage string `ms:"client_away_message"`
-}
-
 func main() {
 	r := mux.NewRouter()
 	updated := time.Now()
 
-	var channels []*Channel
-	var users []*ClientInfo
+	var channels []*ts3.Channel
+	var users []*ts3.OnlineClient
+	var serverInfo *ts3.Server
 
-	t, err := template.New("users").Funcs(map[string]interface{}{"yes": func(b bool) string {
-		if b {
-			return "yes"
-		} else {
-			return "no"
-		}
-	}, "bytes": func(i int) string { return humanize.Bytes(uint64(i)) }}).Parse(htmlTemplate)
+	t, err := template.New("users").Funcs(map[string]interface{}{
+		"yes": func(b bool) string {
+			if b {
+				return "yes"
+			} else {
+				return "no"
+			}
+		},
+		"secondsToDuration": func(s int) string {
+			return (time.Duration(s) * time.Second).String()
+		},
+		"add": func(i, j int) int {
+			return i + j
+		},
+	}).Parse(htmlTemplate)
 
 	if err != nil {
 		panic(err)
@@ -134,6 +56,7 @@ func main() {
 			"channels": channels,
 			"users":    users,
 			"updated":  updated.Format(time.RFC1123),
+			"server":   serverInfo,
 		})
 
 		if err != nil {
@@ -143,7 +66,7 @@ func main() {
 
 	go func() {
 		for {
-			updatedChannels, updatedUsers, err := update()
+			server, updatedChannels, updatedUsers, err := update()
 
 			if err != nil {
 				logrus.Errorf("Unable to update, err: %s", err.Error())
@@ -153,6 +76,7 @@ func main() {
 			channels = updatedChannels
 			users = updatedUsers
 			updated = time.Now()
+			serverInfo = server
 
 			time.Sleep(5 * time.Second)
 		}
@@ -163,76 +87,66 @@ func main() {
 	http.ListenAndServe("0.0.0.0:2208", r)
 }
 
-func update() ([]*Channel, []*ClientInfo, error) {
+func update() (*ts3.Server, []*ts3.Channel, []*ts3.OnlineClient, error) {
 	client, err := ts3.NewClient(server + ":" + port)
 
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
+	defer client.Close()
+
 	cmds := &ts3.ServerMethods{Client: client}
-	defer cmds.Close()
 
 	u, _ := strconv.ParseInt(serverID, 0, 10)
 
-	time.Sleep(1 * time.Second)
-
 	if err := cmds.Use(int(u)); err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
-
-	time.Sleep(1 * time.Second)
 
 	err = cmds.Login(username, password)
 
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
-	var clientList []*OnlineClient
-	if _, err := cmds.ExecCmd(ts3.NewCmd("clientlist").WithResponse(&clientList)); err != nil {
-		return nil, nil, err
+	serverInfo, err := cmds.Info()
+
+	if err != nil {
+		return nil, nil, nil, err
 	}
 
-	time.Sleep(1 * time.Second)
+	clientList, err := cmds.ClientList(ts3.ClientListFull)
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
 
 	if len(clientList) == 0 {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
-	var channels []*Channel
+	channels, err := cmds.ChannelList()
 
-	if _, err := cmds.ExecCmd(ts3.NewCmd("channellist").WithResponse(&channels)); err != nil {
-		return nil, nil, err
+	if err != nil {
+		return nil, nil, nil, err
 	}
 
-	time.Sleep(1 * time.Second)
-
-	clientInfo := make([]*ClientInfo, 0)
+	var clientInfo []*ts3.OnlineClient
 
 	for _, c := range clientList {
-		var cl *ClientInfo
-
-		_, err := cmds.ExecCmd(ts3.NewCmd("clientinfo").WithArgs(ts3.NewArg("clid", c.ID)).WithResponse(&cl))
-
-		if err != nil {
-			return nil, nil, err
-		}
-
-		time.Sleep(1 * time.Second)
-
-		if strings.Contains(cl.Platform, "ServerQuery") {
+		if strings.Contains(*c.Platform, "ServerQuery") {
 			continue
 		}
 
-		clientInfo = append(clientInfo, cl)
+		clientInfo = append(clientInfo, c)
 	}
 
 	sort.Slice(clientInfo, func(i, j int) bool {
 		return clientInfo[i].Nickname < clientInfo[j].Nickname
 	})
 
-	return channels, clientInfo, nil
+	return serverInfo, channels, clientInfo, nil
 }
 
 //go:embed html/index.html
